41388914c9b7b26d21f4621f01a05b6c4db3d49f
Marko Prodanovic <marko.prodanovic@outlook.com>
2024-02-01 12:24:13 -0800

    Updated getBaseURL function

--- src/app/utils/getBaseUrl.tsx
+++ src/app/utils/getBaseUrl.tsx
@@ -1,6 +1,6 @@
 export const getBaseUrl = () => {
   if (process.env.NEXT_PUBLIC_VERCEL_ENV === "production")
-    return "https://markoprodanovic.com";
+    return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
   if (process.env.NEXT_PUBLIC_VERCEL_ENV === "preview")
     return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
   return "http://localhost:3000";
